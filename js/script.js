2bb19dd847f41b8d6e71979f6bd149eb5a80b0a5
guzuro <guzurogmn@gmail.com>
2021-01-04 16:44:14 +0300

    team section, sm fixes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
+   $('.team__image').on("click", function () {
+      $('.team__image').removeClass('team__image_active').eq($(this).index()).addClass('team__image_active')
 
-   $('.carousel').carousel({
-      interval:50000000
+      $('.team__block').removeClass('team__block_active').eq($(this).index()).addClass('team__block_active')
    })
 
+
+   $('.carousel').carousel({
+      interval: 5000000
+    })
+    
 });
+
